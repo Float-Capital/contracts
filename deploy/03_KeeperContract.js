f8c95c81fe614e65f19db1afc566d742aac5e1c9
float-capital <floatcapital@protonmail.com>
2022-01-29 07:32:44 +0000

    Update from https://github.com/Float-Capital/monorepo/commit/516ffc9a36224f246b7a165a23b9c5a74dd1b30a

--- deploy/03_KeeperContract.js
+++ deploy/03_KeeperContract.js
@@ -1,4 +1,107 @@
+const { config } = require("dotenv");
 const { LONGSHORT, STAKER } = require("../helper-hardhat-config");
+
+let networkToUse = network.name;
+
+if (!!process.env.HARDHAT_FORK) {
+  networkToUse = process.env.HARDHAT_FORK;
+}
+
+const oneHour = 3600; // 3600 = every hour
+const twoHours = 7200; // 7200 = every 2 hours
+const fourHours = 14400; // 7200 = every 2 hours
+const sixHours = 21600; // 21600 = every 6 hours
+const twelveHours = 43200; // 43200 = every 12 hours
+
+const quarterPercent = "2500000000000000" // 2.5*10^15 is a price change of 0.25%
+const halfPercent = "5000000000000000" // 5*10^15 is a price change of 0.5%
+
+
+const pollygonConfig = [
+  {
+    marketIndex: 1,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 2,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 3,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 4,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 5,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 6,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 7,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 8,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 9,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 10,
+    heartbeat: fourHours, deviationThreshold: halfPercent
+  }
+]
+
+const avalancheConfig = [
+  {
+    marketIndex: 1,
+    heartbeat: sixHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 2,
+    heartbeat: sixHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 3,
+    heartbeat: sixHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 4,
+    heartbeat: sixHours, deviationThreshold: halfPercent
+  },
+  {
+    marketIndex: 5,
+    heartbeat: sixHours, deviationThreshold: halfPercent
+  }
+]
+
+let updateKeeperConfig = async (keeperContract, marketIndex, heartbeat, deviationThreshold) => {
+  const currentHeartbeat = await keeperContract.updateTimeThresholdInSeconds(marketIndex)
+
+  if (currentHeartbeat.eq(ethers.BigNumber.from(heartbeat))) {
+    console.log("heartbeat set correctly for market", marketIndex)
+  } else {
+    console.log(`heartbeat incorrect for market ${marketIndex} - should be ${heartbeat.toString()} but it is currently ${currentHeartbeat.toString()}`)
+    await keeperContract.updateHeartbeatThresholdForMarket(marketIndex, heartbeat)
+  }
+
+  const currentDeviationThreshold = await keeperContract.percentChangeThreshold(marketIndex)
+
+  if (currentDeviationThreshold.eq(ethers.BigNumber.from(deviationThreshold))) {
+    console.log("deviation threshold set correctly for market", marketIndex)
+  } else {
+    console.log(`deviation threshold incorrect for market ${marketIndex} - should be ${deviationThreshold.toString()} but it is currently ${currentDeviationThreshold.toString()}`)
+    await keeperContract.updatePercentChangeThresholdForMarket(marketIndex, deviationThreshold)
+  }
+}
 
 module.exports = async ({ deployments }) => {
   const { deploy } = deployments;
@@ -6,9 +109,6 @@ module.exports = async ({ deployments }) => {
   const accounts = await ethers.getSigners();
 
   const deployer = accounts[0].address;
-
-  const longShort = await deployments.get(LONGSHORT);
-  const staker = await deployments.get(STAKER);
 
   console.log("Deploying contracts with the account:", deployer);
   console.log(deployer);
@@ -34,35 +134,18 @@ module.exports = async ({ deployments }) => {
     keeper.address
   );
 
-  // 43200 = every 12 hours
-  // 3600 = every hour
-  // 21600 = every 6 hours
-  // await keeperContract.updatePercentChangeThresholdForMarket(
-  //   1,
-  //   "25000000000000000" // 2.5*10^15 is a price change of 0.25%
-  // )
-  /* 
-    await keeperContract.updateHeartbeatThresholdForMarket(2, 3600) // in times of extremely low volotility and no user interaction only update every hour
-    await keeperContract.updatePercentChangeThresholdForMarket(
-      2,
-      "2500000000000000" // 2.5*10^15 is a price change of 0.25%
-    )
-    console.log("market 2 configured")
-  
-    await keeperContract.updateHeartbeatThresholdForMarket(3, 3600) // in times of extremely low volotility and no user interaction only update every hour
-    await keeperContract.updatePercentChangeThresholdForMarket(
-      3,
-      "2500000000000000" // 2.5*10^15 is a price change of 0.25%
-    )
-    console.log("market 3 configured")
-    await keeperContract.updateHeartbeatThresholdForMarket(4, 3600) // in times of extremely low volotility and no user interaction only update every hour
-    await keeperContract.updatePercentChangeThresholdForMarket(
-      4,
-      "2500000000000000" // 2.5*10^15 is a price change of 0.25%
-    )
-    
-    Configured up to market 5 for avalanche! 6 hours and  0.25%
-    Configured up to market 7 for avalanche! 1 hour  and  0.25%
-    */
+  console.log("keeper address", keeper.address)
+
+  if (networkToUse === "polygon") {
+    for ({ marketIndex, heartbeat, deviationThreshold } of pollygonConfig) {
+      await updateKeeperConfig(keeperContract, marketIndex, heartbeat, deviationThreshold)
+    }
+  } else if (networkToUse === "avalanche") {
+    for ({ marketIndex, heartbeat, deviationThreshold } of avalancheConfig) {
+      await updateKeeperConfig(keeperContract, marketIndex, heartbeat, deviationThreshold)
+    }
+  } else {
+    throw new Error("keeper config not defined for this network")
+  }
 };
 module.exports.tags = ["keeper"];
